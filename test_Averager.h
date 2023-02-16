866e8426c33adf027f46a9d2e93f0014d7098da3
IP-Cam <cam@industrialplankton.com>
2023-02-16 09:55:05 -0800

    Feat: add unity test

---
+++ test_Averager.h
@@ -0,0 +1,19 @@
+#include <Averager.h>
+#include "unity.h"
+
+void testAverager()
+{
+    Averager<int, long> TestAverager(10);
+    TEST_ASSERT_EQUAL(0, TestAverager.Average());
+    for (int i = 0; i < 20; ++i)
+    {
+        TEST_ASSERT_EQUAL(5, TestAverager.push(5));
+        TEST_ASSERT_EQUAL(5, TestAverager.Average());
+    }
+    TEST_ASSERT_EQUAL(50, TestAverager.Sum());
+}
+
+void TestAverager(void)
+{
+    RUN_TEST(testAverager);
+}
